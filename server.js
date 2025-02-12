d27913a641c4b2cd78457da401dbb80e559b3bd5
sido69 <yoann.croguennec@gmail.com>
2025-02-12 23:56:04 +0100

    first commit

--- server.js
+++ server.js
@@ -62,3 +62,47 @@ app.post("/habits", async (req, res) => {
     res.status(500).json({ error: "Network error" });
   }
 });
+
+
+app.get("/habitslist", async (req, res) => {
+  try {
+    const allHabits = await Habit.find({});
+
+    res.status(200).json(allHabits);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.put("/habits/:habitId/completed", async (req, res) => {
+  const habitId = req.params.habitId;
+  const updatedCompletion = req.body.completed; // The updated completion object
+
+  try {
+    const updatedHabit = await Habit.findByIdAndUpdate(
+      habitId,
+      { completed: updatedCompletion },
+      { new: true }
+    );
+
+    if (!updatedHabit) {
+      return res.status(404).json({ error: "Habit not found" });
+    }
+
+    return res.status(200).json(updatedHabit);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+app.delete("/habits/:habitId", async (req, res) => {
+  try {
+    const { habitId } = req.params;
+
+    await Habit.findByIdAndDelete(habitId);
+
+    res.status(200).json({ message: "Habit deleted succusfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Unable to delete the habit" });
+  }
+});
